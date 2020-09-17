de996ddd8dfdda64a9f745e5bb27cffbcd7a6665
andrsonk <andrkevich@ukr.net>
2020-09-17 09:53:55 +0300

    Оновлення

--- Program.cs
+++ Program.cs
@@ -9,6 +9,5 @@ namespace TRPZ2
             Console.WriteLine("� 5 Horovenko");
             Console.WriteLine("9 - Klymov");
             Console.WriteLine("Kolomiets Anya "+"13");
-        }
-    }
+            Console.WriteLine("№3 Kate Babanina");
 }
