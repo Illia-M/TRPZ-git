66ac4ac80fcff524bc889de619b584f4371f47b3
Dm_Grt <grytsishin@gmail.com>
2020-09-17 09:18:02 +0300

    Added name writing

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TRPZ2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("IA-81 Hrytsyshyn Dmytro №07!");
         }
     }
 }
