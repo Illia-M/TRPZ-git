9c018619a374cca488b8ef2cff7f5bb67f1ef595
Egor <gorban.eg@gmail.com>
2020-09-17 09:21:08 +0300

    Egor Gorban

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TRPZ2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Number: 4, Egor Gorban");
         }
     }
 }
