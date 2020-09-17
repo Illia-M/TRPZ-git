6f7cd179e596060e683feae4d4b1ebb08c15144c
Evgeniy <sereda0316@gmail.com>
2020-09-17 09:48:58 +0300

    add name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TRPZ2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sereda Yevgen, 24");
         }
     }
 }
