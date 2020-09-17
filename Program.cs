69473e63c6e9878ff3c143e5b676aec6f7697a40
DMYTRO <GorovDimon11>
2020-09-17 10:05:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace TRPZ2
         {
             Console.WriteLine("12 Petro Kolodko");
             Console.WriteLine("IA-81 Hrytsyshyn Dmytro №07!");
-            Console.WriteLine("� 5 Horovenko");
+            Console.WriteLine("� 5 Horovenko");
             Console.WriteLine("Kolomietz Anna " + 13);
             Console.WriteLine("Sereda Yevgen, 24");
             Console.WriteLine("№3 Kate Babanina");
