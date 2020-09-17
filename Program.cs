3c56ec4273eb99ed540631947c1b2dac259c03a1
Illia Maier <illia.maier@pm.bet>
2020-09-17 09:44:13 +0300

    Revert "change file"
    This reverts commit b9b218c171b99cf06679689f9598f0b499591bc3.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TRPZ2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kolomiets Anya "+"13");
+            Console.WriteLine("Hello World!");
         }
     }
 }
