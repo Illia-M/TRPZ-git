046955961f19505934a5faa9db94c87f8ed17261
Zev-Arthur <zev.arthur@gmail.com>
2020-09-30 13:01:37 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 print('Hello, IA-82!')
 print('#1 FirstName LastName')
 print('#6 Kateryna Drobatii')
+print('#8 Arthur Zevaloff')
 print('#11 Nastya Kovalenko')
 print ( '# 20 Денис Паршуткин' )
 print ( '# Savina' )
