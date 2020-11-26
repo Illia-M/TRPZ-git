f6a522523d86984e40d9c96620843ee52ecebd93
Illia Maier <illia.maier@pm.bet>
2020-11-26 09:06:16 +0200

    Add task

---
+++ Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("#1 FirstName LastName")
+    End Sub
+End Module
