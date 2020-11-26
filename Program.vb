81751c07e977f49b1188e26e0d7dc45804b31d66
Dzirael <kyryanmet@gmail.com>
2020-11-26 09:49:27 +0200

    Змінений текст

--- Program.vb
+++
@@ -1,8 +0,0 @@
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("#1 Kyrylo Tymchenko")
-    End Sub
-End Module
