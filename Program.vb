f9b54e7190fca11b8df34480310e75232fe6a341
Dzirael <kyryanmet@gmail.com>
2020-11-26 09:55:57 +0200

    Змінений текст

---
+++ Program.vb
@@ -0,0 +1,14 @@
+diff --git a/Program.vb b/Program.vb
+deleted file mode 100644
+index e4813f4..0000000
+--- a/Program.vb
++++ /dev/null
+@@ -1,8 +0,0 @@
+-Imports System
+-
+-Module Program
+-    Sub Main(args As String())
+-        Console.WriteLine("Hello World!")
+-        Console.WriteLine("#1 Kyrylo Tymchenko")
+-    End Sub
+-End Module
