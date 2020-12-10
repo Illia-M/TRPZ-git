ac1427d81d3c5230cf52a1f77834cfeab0093c20
lexivashina <alexan.ivashina@gmail.com>
2020-12-10 10:18:51 +0200

    New branch add IA-81-4

--- Program.vb
+++
@@ -1,8 +0,0 @@
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("#1 FirstName LastName")
-    End Sub
-End Module
