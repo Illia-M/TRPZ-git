b8de20f6e1691c7bdd9fbd0591dfaa35773403dd
lexivashina <alexan.ivashina@gmail.com>
2020-12-10 10:10:53 +0200

    Console.WriteLine("#2 Lyashenko Polina") add

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("#1 FirstName LastName")
+        Console.WriteLine("#2 Lyashenko Polina")
     End Sub
 End Module
