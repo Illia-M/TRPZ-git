c7c865cb6a5fdcabbffc8519adc595bf1cdc7fcf
v.makarov <viollo190201@gmail.com>
2020-12-23 10:14:56 +0200

    Add my FN and SN

--- Controllers/WeatherForecastController.fs
+++ Controllers/WeatherForecastController.fs
@@ -18,7 +18,7 @@ type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
             "1. FirstName LastName"
 		Vlad Ulizko
             "1. FirstName LastName"
-            "2. FirstName LastName"
+            "2. Valeriy Makarov"
             "1. Vlad Ulizko"		
             "2. Vlad Shapkin"
             "3. FirstName LastName"
