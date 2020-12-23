5604ad537a1f483da34be72394b1659c188a6431
Illia Maier <illia.maier@pm.bet>
2020-12-23 09:05:30 +0200

    Add F# WebApi task

---
+++ Controllers/WeatherForecastController.fs
@@ -0,0 +1,34 @@
+﻿namespace TRPZ2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open TRPZ2
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    let summaries =
+        [|
+            "1. FirstName LastName"
+            "2. FirstName LastName"
+            "3. FirstName LastName"
+            "4. FirstName LastName"
+            "5. FirstName LastName"
+            "6. FirstName LastName"
+        |]
+
+    [<HttpGet>]
+    member _.Get() =
+        let rng = System.Random()
+        [|
+            for index in 0..4 ->
+                { Date = DateTime.Now.AddDays(float index)
+                  TemperatureC = rng.Next(-20,55)
+                  Summary = summaries.[rng.Next(summaries.Length)] }
+        |]
