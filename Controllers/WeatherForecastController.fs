91d4663175fa8d41e8ebd29b0c97390f37a90408
Covielli <66259576+Covielli@users.noreply.github.com>
2020-12-24 09:50:47 +0200

    Update WeatherForecastController.fs

--- Controllers/WeatherForecastController.fs
+++ Controllers/WeatherForecastController.fs
@@ -17,7 +17,7 @@ type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
         [|
             "1. FirstName LastName"
             "2. FirstName LastName"
-            "3. FirstName LastName"
+            "3. Horoshilov Bogdan"
             "4. FirstName LastName"
             "5. FirstName LastName"
             "6. FirstName LastName"
