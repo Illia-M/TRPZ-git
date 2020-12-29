386021cb27ae569658a0c10f2897af2e657d8b80
Dzirael <kyryanmet@gmail.com>
2020-12-29 10:17:37 +0200

    Update GreeterService.cs

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -19,7 +19,8 @@ namespace TRPZ2
         {
             var studentsList = new[]
             {
-                "1. Tymchenko Kyrylo"
+                "1. Andrei Chernya"
+                "2. Tymchenko Kyrylo"
             };
             
             return Task.FromResult(new HelloReply
