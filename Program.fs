119b2dc201c1ac76f60eb43eed84e817185d271a
Illia Maier <illia.maier@pm.bet>
2021-09-15 09:20:55 +0300

    Add F# task

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let name1 = from "FirstName LastName!" // Call the function
+    printfn "Hello world %s" name1
+    0 // return an integer exit code
