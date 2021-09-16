77e56d506f8f83522f2eed230c1069b29c39d3cb
UlianaDzigil <68972045+UlianaDzigil@users.noreply.github.com>
2021-09-16 09:51:35 +0300

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Dzigil Uliana")
+        Console.WriteLine("Dzigil Uliana")
     End Sub
 End Module
