8a528d37eab34ab1675b81f662ad8b1e3e5b55db
TechnieLuna() <36112723+VisualX2@users.noreply.github.com>
2021-09-16 09:05:34 +0300

    Returned Serhii

--- Program.vb
+++ Program.vb
@@ -3,6 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Ovchynnikov Serhii")
         Console.WriteLine("Mashkov Andriy")
     End Sub
 End Module
