2cd77c464665c80d351b0199a1040c8c00bc15f8
c1nnam0nbun <c1nnam0nbun270@gmail.com>
2021-09-16 08:51:27 +0300

    Output set to 'Andrei Ishchenko'

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("FirstName LastName")
+        Console.WriteLine("Andrei Ishchenko")
     End Sub
 End Module
