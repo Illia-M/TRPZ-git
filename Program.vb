4810efe33b3423d3e5d75223adf89ed873823522
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2021-09-16 09:05:49 +0300

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Sergey Ovchinnikov")
+		Console.WriteLine("Nikita Linovytskyi")
     End Sub
 End Module
