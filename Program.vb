2d62e420ad490ea773ec82b7ce97a194b3ca4fad
Anya Morhunova <fyz12vjhueyjdf@gmail.com>
2021-09-16 09:14:11 +0300

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Sergey Ovchinnikov")
+        Console.WriteLine("Anna Morhunova")
     End Sub
 End Module
