1a0139423dea03dd301863c42f80610ecc1d633f
TechnieLuna() <36112723+VisualX2@users.noreply.github.com>
2021-09-16 09:00:23 +0300

    Task

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Sergey Ovchinnikov")
+        Console.WriteLine("Mashkov Andriy")
     End Sub
 End Module
