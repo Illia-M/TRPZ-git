bc15f5c622fc2695646cbaaf99c585c871f6bfa5
AllroadDev <sasha110102@gmail.com>
2021-09-16 09:50:44 +0300

    1234

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Bezsmertnyi Oleksandr")
+	Console.WriteLine("Bezsmertnyi Oleksandr")
     End Sub
 End Module
