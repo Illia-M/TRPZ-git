331d3036346b72632897c544c0b632f0a2cfe72d
Mezhuiev2002 <maxomka77@gmail.com>
2021-09-22 09:57:19 +0300

    Adding my firstname and lastName

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello World")
+
+print("Ilona Mykhailova")
 print("FirstName LastName")
+print("Serhii Basik")
 print("Artem Bystritskyi")
+print("Ivashchenko Daniil")
+print('Oleksiy Didenko')
+print('Mezhuiev Maksym')
