07a0eafcf2b3dc63ef00d4b3d359224e190dc3bb
Udjiniia <jeniagryaznova@ukr.net>
2021-09-23 09:43:11 +0300

    lab1

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
     fmt.Println("hello world")
+
     fmt.Println("Hriaznova Yevheniia")
     fmt.Println("Denis Vasylyev")
     fmt.Println("FirstName LastName")
@@ -29,6 +30,18 @@ func main() {
 	fmt.Println("Maksym Chalyi")
     fmt.Println("Karyna Khotin")
 
-}
+    fmt.Println("Denis Vasylyev")
+    fmt.Println("FirstName LastName")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
 
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Denys Suk")
+    fmt.Println("Khrystyna Ambroziak")
+    fmt.Println("Sofia Trokhymtchuk")
+	fmt.Println("Vladyslav Polishchuk")
+	fmt.Println("Maksym Chalyi")
+    fmt.Println("Karyna Khotin")
+}
 
