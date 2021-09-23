860cadd4bd5ffa0a1b5d8cc0353627be66637310
Petro14 <kuchma200217@gmail.com>
2021-09-23 09:35:22 +0300

    final

--- hello.go
+++ hello.go
@@ -1,8 +1,22 @@
 package main
 import "fmt"
+
 func main() {
     fmt.Println("hello world")
+    fmt.Println("Denis Vasylyev")
+    fmt.Println("FirstName LastName")
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Denys Suk")
+    fmt.Println("Khrystyna Ambroziak")
+
+    fmt.Println("Sofia Trokhymtchuk")
+	fmt.Println("Vladyslav Polishchuk")
+	fmt.Println("Maksym Chalyi")
+    fmt.Println("Karyna Khotin")
+
+
     fmt.Println("Petro Herenshtein")
 }
