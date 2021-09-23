e1e7880e6466ee50d544d3326120ebee96583145
Dumes <dimaarseniyk@gmail.com>
2021-09-23 09:44:06 +0300

    Lab1Ars

--- hello.go
+++ hello.go
@@ -3,20 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
-    fmt.Println("Denis Vasylyev")
+    fmt.Println("hello world") 
     fmt.Println("FirstName LastName")
+    fmt.Println("Oleksandr Kuzmenko")
+    fmt.Println("Denis Vasylyev")
     fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Volodymyr Chumak")   
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
-
     fmt.Println("Sofia Trokhymtchuk")
 	fmt.Println("Vladyslav Polishchuk")
+	fmt.Println("Evheniy Volyk")
 	fmt.Println("Maksym Chalyi")
     fmt.Println("Karyna Khotin")
-	fmt.Println("Arseniyk Dmytro")
+	fmt.Println("Dmytro Arseniyk")
 }
   
