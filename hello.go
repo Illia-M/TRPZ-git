242e35448324dbfbcf581ca34cfd00466a99d5d5
Udjiniia <jeniagryaznova@ukr.net>
2021-09-23 09:53:44 +0300

    lab1

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
     fmt.Println("hello world")
 
     fmt.Println("Hriaznova Yevheniia")
@@ -22,6 +23,7 @@ func main() {
      fmt.Println("Hriaznova Yevheniia")
     fmt.Println("Volodymyr Chumak")
     fmt.Println("Yaroslav Raksha")
+<<<<<<< Updated upstream
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
     fmt.Println("Denys Suk")
@@ -38,12 +40,28 @@ func main() {
 
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
+=======
+
+    fmt.Println("hello world") 
+    fmt.Println("FirstName LastName")
+    fmt.Println("Oleksandr Kuzmenko")
+    fmt.Println("Denis Vasylyev")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")   
+>>>>>>> Stashed changes
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
     fmt.Println("Sofia Trokhymtchuk")
 	fmt.Println("Vladyslav Polishchuk")
+<<<<<<< Updated upstream
 	fmt.Println("Maksym Chalyi")
     fmt.Println("Karyna Khotin")
 
+=======
+	fmt.Println("Evheniy Volyk")
+	fmt.Println("Maksym Chalyi")
+    fmt.Println("Karyna Khotin")
+>>>>>>> Stashed changes
 }
+  
 
