b7a3f16a9bf00716106de3bb6e48d0fa73f6cda9
Udjiniia <jeniagryaznova@ukr.net>
2021-09-23 09:21:07 +0300

    lab1

--- hello.go
+++ hello.go
@@ -1,6 +1,22 @@
 package main
+
 import "fmt"
+
 func main() {
     fmt.Println("hello world")
     fmt.Println("Hriaznova Yevheniia")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("FirstName LastName")
+    fmt.Println("Oleksandr Kuzmenko")
+    fmt.Println("FirstName LastName")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Denys Suk")
+    fmt.Println("Khrystyna Ambroziak")
+     fmt.Println("Hriaznova Yevheniia")
+    
 }
+>>>>>>> d7bec043a7dfbec4ae7ff5181ead709408a90fc5
+>>>>>>> Stashed changes
