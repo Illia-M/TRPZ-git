595b028a5513b0b0df473ca9f93367148bce6f65
Udjiniia <jeniagryaznova@ukr.net>
2021-09-23 10:02:34 +0300

    lab1

--- hello.go
+++ hello.go
@@ -3,65 +3,19 @@ package main
 import "fmt"
 
 func main() {
-
     fmt.Println("hello world")
-
-    fmt.Println("Hriaznova Yevheniia")
-    fmt.Println("Denis Vasylyev")
-    fmt.Println("FirstName LastName")
-    fmt.Println("Sofiia Fedor")
-
     fmt.Println("FirstName LastName")
     fmt.Println("Oleksandr Kuzmenko")
-    fmt.Println("FirstName LastName")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Denys Suk")
-    fmt.Println("Khrystyna Ambroziak")
-     fmt.Println("Hriaznova Yevheniia")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Yaroslav Raksha")
-<<<<<<< Updated upstream
+    fmt.Println("Denis Vasylyev")
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
     fmt.Println("Sofia Trokhymtchuk")
 	fmt.Println("Vladyslav Polishchuk")
-	fmt.Println("Maksym Chalyi")
-    fmt.Println("Karyna Khotin")
-
-    fmt.Println("Denis Vasylyev")
-    fmt.Println("FirstName LastName")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-=======
-
-    fmt.Println("hello world") 
-    fmt.Println("FirstName LastName")
-    fmt.Println("Oleksandr Kuzmenko")
-    fmt.Println("Denis Vasylyev")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")   
->>>>>>> Stashed changes
-    fmt.Println("Denys Suk")
-    fmt.Println("Khrystyna Ambroziak")
-    fmt.Println("Sofia Trokhymtchuk")
-	fmt.Println("Vladyslav Polishchuk")
-<<<<<<< Updated upstream
-	fmt.Println("Maksym Chalyi")
-    fmt.Println("Karyna Khotin")
-
-=======
 	fmt.Println("Evheniy Volyk")
 	fmt.Println("Maksym Chalyi")
     fmt.Println("Karyna Khotin")
->>>>>>> Stashed changes
+    fmt.Println("Petro Herenshtein")
+    fmt.Println("Hriaznova")
 }
-  
-
