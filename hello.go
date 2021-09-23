5815e9e864468658d0d1009b6c5ac36455f39a10
Illia Maier <illia.maier@pm.bet>
2021-09-23 09:10:23 +0300

    Fixes

--- hello.go
+++ hello.go
@@ -2,12 +2,7 @@ package main
 import "fmt"
 func main() {
     fmt.Println("hello world")
-<<<<<<< HEAD
     fmt.Println("FirstName LastName")
-=======
     fmt.Println("Sofiia Fedor")
->>>>>>> 37315510d27fda63e1d61c12a85009a47335edfe
-
-
-    fmt.Println("Oleksandr Kuzmenko")
+    fmt.Println("Volodymyr Chumak")
 }
