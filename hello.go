54340aa64b34481b863dfbc794af9c2793cf5bc3
VladySlave-P <vlad.polishuk.007@gmail.com>
2021-09-23 09:20:32 +0300

    asd

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
     fmt.Println("hello world")
+<<<<<<< HEAD
     fmt.Println("FirstName LastName")
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
@@ -11,5 +12,6 @@ func main() {
     fmt.Println("Volodymyr Chumak")
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
+	fmt.Println("Vladyslav Polishchuk")
     
 }
