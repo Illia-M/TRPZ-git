6a4c292f92781821d17468b1cc4a57a2f871138f
Yaroslav <rakshayaroslav37@gmail.com>
2021-09-23 09:35:20 +0300

    Yaroslav Raksha IA-94 first lab commit

--- hello.go
+++ hello.go
@@ -3,15 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
-<<<<<<< HEAD
-    fmt.Println("FirstName LastName")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Denys Suk")
-    fmt.Println("Khrystyna Ambroziak")
-	fmt.Println("Vladyslav Polishchuk")
     fmt.Println("Yaroslav Raksha")
 }
