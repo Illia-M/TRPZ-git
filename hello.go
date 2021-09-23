091869aaa889e92b8f83dd17ac4b91df46036f13
Wionixx <troll8182@yandex.ru>
2021-09-23 09:29:26 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -4,14 +4,5 @@ import "fmt"
 
 func main() {
     fmt.Println("hello world")
-    fmt.Println("FirstName LastName")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-
-	
+    fmt.Println("Maksym Chalyi")
 }
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
-    fmt.Println("Denys Suk")
-    fmt.Println("Khrystyna Ambroziak")
-	fmt.Println("Maksym Chalyi")
