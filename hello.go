f3bca3b6957780a3a47634e72339c447a4acfbe2
Udjiniia <jeniagryaznova@ukr.net>
2021-09-23 09:36:18 +0300

    Merge branch 'stream/IA-94-2' of https://github.com/Illia-M/TRPZ-git into stream/IA-94-2

--- hello.go
+++ hello.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
     fmt.Println("hello world")
     fmt.Println("Hriaznova Yevheniia")
+    fmt.Println("Denis Vasylyev")
+    fmt.Println("FirstName LastName")
     fmt.Println("Sofiia Fedor")
     fmt.Println("FirstName LastName")
     fmt.Println("Oleksandr Kuzmenko")
@@ -16,6 +18,17 @@ func main() {
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
      fmt.Println("Hriaznova Yevheniia")
-    
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Yaroslav Raksha")
+    fmt.Println("Sofiia Fedor")
+    fmt.Println("Volodymyr Chumak")
+    fmt.Println("Denys Suk")
+    fmt.Println("Khrystyna Ambroziak")
+    fmt.Println("Sofia Trokhymtchuk")
+	fmt.Println("Vladyslav Polishchuk")
+	fmt.Println("Maksym Chalyi")
+    fmt.Println("Karyna Khotin")
+
 }
+
 
