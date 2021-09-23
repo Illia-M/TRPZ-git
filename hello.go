94619e7a2382bcecf0d2ff10d8b1909b31ea1d05
KhrystynaAA <ambrozyak20@gmail.com>
2021-09-23 09:24:32 +0300

    added line

--- hello.go
+++ hello.go
@@ -7,8 +7,6 @@ func main() {
     fmt.Println("FirstName LastName")
     fmt.Println("Sofiia Fedor")
     fmt.Println("Volodymyr Chumak")
-    fmt.Println("Sofiia Fedor")
-    fmt.Println("Volodymyr Chumak")
     fmt.Println("Denys Suk")
     fmt.Println("Khrystyna Ambroziak")
     
