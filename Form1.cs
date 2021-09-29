a4dc2b4e2e3b926e052123f647f4b341d8000b36
Suspense <55453335+Suspensium@users.noreply.github.com>
2021-09-29 09:34:25 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -25,6 +25,7 @@ namespace TRPZ2
             listBox1.Items.Add("Kateryna Artemenko");
             listBox1.Items.Add("Anatolii Dudik");
             listBox1.Items.Add("Oleh Yermak");
+            listBox1.Items.Add("Vitaliy Smorodinov");
         }
 
     }
