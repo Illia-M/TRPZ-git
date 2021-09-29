21119dc6928213eab3a5235e6a98bab5e1820884
AndrewChikrii <andrey.chikrii@gmail.com>
2021-09-29 09:05:02 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ namespace TRPZ2
         public Form1()
         {
             InitializeComponent();
-            listBox1.Items.Add("FirstName LastName");
+            listBox1.Items.Add("Andrew Chikrii");
         }
 
     }
