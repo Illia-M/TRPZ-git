84e136ca4349462929fd40034cee827a07c98321
kkotuha <artkaya7@gmail.com>
2021-09-29 09:23:00 +0300

    Update Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRPZ2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.Items.Add("FirstName LastName");
+            listBox1.Items.Add("HriaznovaYevhennia");
+            listBox1.Items.Add("Danil Shapovalov");
+            listBox1.Items.Add("Andrew Chikrii");
+            listBox1.Items.Add("Ihor Ponomarov");
+           listBox1.Items.Add("Kateryna Artemenko");
+        }
+
+    }
+}
