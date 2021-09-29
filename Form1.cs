adf295ab6d4e7f8c64a9d08146f0fe3764b33bf1
Eduard <katseduard2002@gmail.com>
2021-09-29 10:32:36 +0300

    Eduard-Kats-commit

--- Form1.cs
+++ Form1.cs
@@ -15,17 +15,7 @@ namespace TRPZ2
         public Form1()
         {
             InitializeComponent();
-            listBox1.Items.Add("FirstName LastName");
-            listBox1.Items.Add("HriaznovaYevhennia");
-            listBox1.Items.Add("Danil Shapovalov");
-            listBox1.Items.Add("Andrew Chikrii");
-            listBox1.Items.Add("Mikriukov Mikhail"); 
-	        listBox1.Items.Add("Polina Sopoleva");
-            listBox1.Items.Add("Stepan Korol");
-            listBox1.Items.Add("Ihor Ponomarov");
-            listBox1.Items.Add("Kateryna Artemenko");
-            listBox1.Items.Add("Vitaliy Smorodinov");
-            listBox1.Items.Add("Alexandr Rolik");
+            listBox1.Items.Add("Eduard Kats");
         }
     }
 }
