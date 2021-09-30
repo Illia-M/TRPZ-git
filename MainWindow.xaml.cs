ea112c9b15d158541cf7cbaffcff2477ba1c033d
Illia Maier <illia.maier@pm.bet>
2021-09-30 08:42:59 +0300

    Add WPF task

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace TRPZ2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lb1.Items.Add("FirstName LastName");
+        }
+    }
+}
