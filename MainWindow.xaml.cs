a82f2e5211cf2b3c00c7a0940c05d5a8771d86e1
login <gulyaschy@gmail.com>
2021-09-30 09:44:59 +0300

    123

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,16 @@ namespace TRPZ2
         public MainWindow()
         {
             InitializeComponent();
+            lb1.Items.Add("Denis Vasylyev IA94");
+            lb1.Items.Add("Vsevolod Zhohan");
+            lb1.Items.Add("Denis Vasylyev IA94");
+            lb1.Items.Add("Andrei Ishchenko");
+            lb1.Items.Add("Alexandr Rolik");
+            lb1.Items.Add("Oleg Yermak");
+            lb1.Items.Add("Maksym Mezhuiev");
+            lb1.Items.Add("Anhelina Liaskovych");
+            lb1.Items.Add("Anhelina Liaskovych"); 
+            lb1.Items.Add("Vlad Minaiev");
             lb1.Items.Add("Igor Gulyaschy");
         }
     }
