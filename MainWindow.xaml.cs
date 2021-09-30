699dc0b073edf20ca24123a31ea0f0a0e75dc570
c1nnam0nbun <c1nnam0nbun270@gmail.com>
2021-09-30 08:58:19 +0300

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,12 +10,8 @@ namespace TRPZ2
         public MainWindow()
         {
             InitializeComponent();
-<<<<<<< HEAD
-            lb1.Items.Add("FirstName LastName");
             lb1.Items.Add("Denis Vasylyev IA94");
-=======
             lb1.Items.Add("Andrei Ishchenko");
->>>>>>> Andrei_Ishchenko_3
         }
     }
 }
