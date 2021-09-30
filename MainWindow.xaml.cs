23429c490f2209f614c18c24b8b79ef141acc69d
Alexandr-Rolik <alexir7762@gmail.com>
2021-09-30 08:54:18 +0300

    rolik

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TRPZ2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lb1.Items.Add("FirstName LastName");
+            lb1.Items.Add("Alexandr Rolik");
+        }
+    }
+}
