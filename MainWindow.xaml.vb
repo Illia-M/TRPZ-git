ac23f0978c00d830b201b1cdeeec16844c87d717
IllyaPashchenko <illya.pashchenko@gmail.com>
2021-10-06 09:32:18 +0300

    pashchenko_commit

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,5 +1,6 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         listBox1.Items.Add("FirstName LastName")
+        listBox1.Items.Add("Illia Pashchenko")
     End Sub
 End Class
