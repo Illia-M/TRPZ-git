e39f47642dbfec780a68310847fccaed50f949a2
kamapuliaa <palij653@gmail.com>
2021-10-06 10:25:13 +0300

    add Kamilla Paliy

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -2,5 +2,6 @@
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
 	    listBox1.Items.Add("Ilya Paschenko")
         listBox1.Items.Add("Andrey Snigur")
+        listBox1.Items.Add("Kamilla Paliy")
     End Sub
 End Class
