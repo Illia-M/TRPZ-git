975b594984652697d050f9a6a6e77803c8883605
gl-hf <andreysnigur2001@gmail.com>
2021-10-06 10:13:34 +0300

    Update MainWindow.xaml.vb

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,5 +1,6 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
+	    listBox1.Items.Add("Ilya Paschenko")
         listBox1.Items.Add("Andrey Snigur")
     End Sub
 End Class
