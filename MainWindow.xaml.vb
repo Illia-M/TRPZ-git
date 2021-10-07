1a958e2877bb7c9ee18c43907e4a03c3faddfc8a
zutnik <54157558+zutnik@users.noreply.github.com>
2021-10-07 09:02:02 +0300

    commit

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,5 +1,5 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
-        listBox1.Items.Add("Andrey Snigur")
+        listBox1.Items.Add("Bogdan Zhytnyk")
     End Sub
 End Class
