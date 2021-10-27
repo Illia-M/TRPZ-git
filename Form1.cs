3ab9afd1efc65fe77216ed674a9584110e189778
Danil <Shramkod13@gmail.com>
2021-10-27 08:54:02 +0300

    Merge branch 'Daniil_Shramko' into stream/IA-92-3

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRPZ2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("FirstName LastName");
+            listBox1.Items.Add("HriaznovaYevhennia");
+            listBox1.Items.Add("Danil Shapovalov");
+            listBox1.Items.Add("Andrew Chikrii");
+            listBox1.Items.Add("Mikriukov Mikhail"); 
+	        listBox1.Items.Add("Polina Sopoleva");
+            listBox1.Items.Add("Stepan Korol");
+            listBox1.Items.Add("Ihor Ponomarov");
+            listBox1.Items.Add("Kateryna Artemenko");
+            listBox1.Items.Add("Vitaliy Smorodinov");
+            listBox1.Items.Add("Alexandr Rolik");
+            listBox1.Items.Add("Eduard Kats");
+        }
+    }
+}
