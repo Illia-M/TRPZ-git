0e458cd43133aa9d712a6ea6552c888c7a7c9fc9
RomanGf <gafyak.roma@gmail.com>
2021-10-28 09:29:00 +0300

    add Roman_Hafiak to labes

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,7 @@ let message: string = 'Hello, World!';
 console.log(message);
 let student: string = 'FirstName LastName';
 console.log(student);
+const student2: string = 'Igor Gulyaschy';
+console.log(student2);
+const student2: string = 'Roman Hafiak';
+console.log(student2);
