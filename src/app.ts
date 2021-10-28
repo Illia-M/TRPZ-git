0142e96902892ec192ee6d26e0753046171b88e0
RomanGf <gafyak.roma@gmail.com>
2021-10-28 10:00:35 +0300

    change

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,11 @@
+
 let message: string = 'Hello, World!';
 console.log(message);
 let student: string = 'FirstName LastName';
 console.log(student);
-<<<<<<< Roman_Hafiak
-const student2: string = 'Igor Gulyaschy';
-console.log(student2);
-const student2: string = 'Alex Karpov';
-console.log(student2);
 const student3: string = 'Roman Hafiak';
 console.log(student3);
-=======
-<<<<<<< HEAD
 const student2: string = 'Alex Karpov';
 console.log(student2);
 const student2: string = 'Igor Gulyaschy';
 console.log(student2);
-=======
-const student2: string = 'Igor Gulyaschy';
-console.log(student2);
-
->>>>>>> b035843908c879753161beeefc4e58558393e664
->>>>>>> stream/IA-91-5
