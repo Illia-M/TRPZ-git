b035843908c879753161beeefc4e58558393e664
login <gulyaschy@gmail.com>
2021-10-28 09:24:09 +0300

    IgorGulyaschy

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,6 @@ let message: string = 'Hello, World!';
 console.log(message);
 let student: string = 'FirstName LastName';
 console.log(student);
+const student2: string = 'Igor Gulyaschy';
+console.log(student2);
+
