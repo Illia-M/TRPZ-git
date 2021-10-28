fb69f53c793c15b646cf729d09ae05d2012820ee
axkarpov <axkarpov@gmail.com>
2021-10-28 09:39:07 +0300

    Alex

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,5 @@ let student: string = 'FirstName LastName';
 console.log(student);
 const student2: string = 'Alex Karpov';
 console.log(student2);
+const student2: string = 'Igor Gulyaschy';
+console.log(student2);
