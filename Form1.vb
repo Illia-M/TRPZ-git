78723f745bce3487b7e576d687be997152801a5f
Dazzzzzzzzz <iamzagoruiko@gmail.com>
2021-11-04 09:43:39 +0200

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ListBox1.Items.Add("FirstName LastName")
+        ListBox1.Items.Add("Zagoruiko Oleksandr")
     End Sub
 End Class
