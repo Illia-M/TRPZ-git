27976f691b31034258d1c7dca28c05fcba8851d6
Michael-Slh <actualhooman@gmail.com>
2021-12-30 17:43:48 +0300

    Update Hello.scala

---
+++ Hello.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+    println("Trach Ruslana")
+    println("Raksha Yaroslav")
+
+    println("Salikhov Michael")
+  }
+}
