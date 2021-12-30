0e3b2d93b786db9e11baab3a8028a3cf96991932
ElfMydak <oapk2002@gmail.com>
2021-12-30 18:46:59 +0200

    Panasiuk

--- hello.adb
+++ hello.adb
@@ -4,5 +4,9 @@ begin
   Put_Line("Hello, world!");
   Put_Line("FirstName LastName");
   Put_Line("Trach Ruslana");
+  Put_Line("Kesil Stas");
+  Put_Line("Lazarenko Artem");
+  Put_Line("Ruslan Slivka");
+  Put_Line("Nazar Omelchuk");
   Put_Line("Oleksandr Panasiuk");
 end Hello;
