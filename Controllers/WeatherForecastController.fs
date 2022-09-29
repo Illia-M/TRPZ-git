bf4227cf9505b9fe58208ed4e27ea1fc05cf4e0a
Jedich <alexyudakov483@gmail.com>
2022-09-29 12:57:54 +0300

    add firstName lastName to WeatherForecastController.fs

--- Controllers/WeatherForecastController.fs
+++ Controllers/WeatherForecastController.fs
@@ -15,12 +15,7 @@ type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
 
     let summaries =
         [|
-            "1. FirstName LastName"
-            "2. FirstName LastName"
-            "3. FirstName LastName"
-            "4. FirstName LastName"
-            "5. FirstName LastName"
-            "6. FirstName LastName"
+            "Oleksandr Yudakov"
         |]
 
     [<HttpGet>]
