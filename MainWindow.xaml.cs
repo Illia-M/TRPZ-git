57524c96ab0d40e05ed980b1e0d7ef309124ab7e
Zexino <great_doter@ukr.net>
2022-09-29 15:33:13 +0300

    Nazar commit fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿ng System.Windows;
 
 namespace TRPZ2
 {
@@ -10,10 +10,26 @@ namespace TRPZ2
         public MainWindow()
         {
             InitializeComponent();
-            lb1.Items.Add("FirstName LastName");
-            lb1.Items.Add("Max Vavryniuk");
+            lb1.Items.Add("Anna Heleshko");
+            lb1.Items.Add("Stas Logvynskyi");
+            lb1.Items.Add("Oleksiy Okorokov");
+            lb1.Items.Add("Dmitrii Nieliepin");
+			lb1.Items.Add("Andrey Kolomets");
+            lb1.Items.Add("Yana Yushchuk");
             lb1.Items.Add("Denys Sorokin");
+            lb1.Items.Add("Danil Melnik");
 			lb1.Items.Add("Knysh Nazar");
+	        lb1.Items.Add("Denys Grynenko");
+		    lb1.Items.Add("Yevhenii Borysenko");
+		    lb1.Items.Add("Max Vavryniuk");
+	        lb1.Items.Add("Volodymyr Danchuk");
+            lb1.Items.Add("Huz Bohdan");
+	        lb1.Items.Add("Denys Grynenko");
+            lb1.Items.Add("Yevhenii Borysenko");
+            lb1.Items.Add("Maksym Melnyk");
+            lb1.Items.Add("Yaroslav Andrushchenko");
+	        lb1.Items.Add("Denys Grynenko");
+		    lb1.Items.Add("Yevhenii Borysenko");
         }
     }
 }
