5f7306eedbc8d74710342556dc483c98f9bc0307
rluCvVWc <panv1t2l1k@gmail.com>
2022-09-29 22:37:09 +1000

    erase odd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,4 @@
-﻿ng System.Windows;
-
-namespace TRPZ2
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            lb1.Items.Add("Vitalii Panchenko");
-        }
-    }
-}
-
-using System.Windows;
+﻿using System.Windows;
 
 namespace TRPZ2
 {
