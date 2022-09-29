4d586a87cd99bb683c6ae88a89bcb7d386f1e3d8
Max <maxvavryniuk@gmail.com>
2022-09-29 15:18:48 +0300

    add/edit line

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,21 +14,19 @@ namespace TRPZ2
             lb1.Items.Add("Stas Logvynskyi");
             lb1.Items.Add("Oleksiy Okorokov");
             lb1.Items.Add("Dmitrii Nieliepin");
-            lb1.Items.Add("Max Vavryniuk");
-			lb1.Items.Add("Andrey Kolomets");
+            lb1.Items.Add("Maxym Vavryniuk");
+	    lb1.Items.Add("Andrey Kolomets");
             lb1.Items.Add("Yana Yushchuk");
             lb1.Items.Add("Denys Sorokin");
-<<<<<<< HEAD
-			lb1.Items.Add("Knysh Nazar");
-=======
+	    lb1.Items.Add("Knysh Nazar");
+
             lb1.Items.Add("Danil Melnik");
 	    lb1.Items.Add("Volodymyr Danchuk");
-<<<<<<< HEAD
-lb1.Items.Add("Huz Bohdan");
-=======
+
+	lb1.Items.Add("Huz Bohdan");
+
 	    lb1.Items.Add("Denys Grynenko");
 
->>>>>>> 8eebdfdefa7957af0fd74e4b1d4f14938133e626
         }
     }
 }
