2b2998f63d60c555d1cd6e79815830b7af07027d
Volodymyr Yakymenko <vovkayakimenko2564@gmail.com>
2022-09-29 15:11:38 +0300

    Volodymyr_Yakymenko

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ namespace TRPZ2
             InitializeComponent();
             lb1.Items.Add("Oleksiy Okorokov");
             lb1.Items.Add("Dmitrii Nieliepin");
+		lb1.Items.Add("Volodymyr Yakymenko");
         }
     }
 }
