c22d2918f88378dc05f1ed885c3a585753e9bef1
Max <maxvavryniuk@gmail.com>
2022-09-29 13:03:53 +0300

    new line

--- Controllers/WeatherForecastController.fs
+++ Controllers/WeatherForecastController.fs
@@ -17,7 +17,7 @@ type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
         [|
             "1. FirstName LastName"
             "2. Oleksandr Yudakov"
-            "3. Max Vavryniuk"
+            "3. Maximka Vavryniuk"
             "4. FirstName LastName"
             "5. FirstName LastName"
             "6. FirstName LastName"
