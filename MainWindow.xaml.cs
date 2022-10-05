67778723979abe2c3d87e22cb806154b7b9cb0a6
Volodymyr Yakymenko <vovkayakimenko2564@gmail.com>
2022-10-05 21:46:34 +0300

    Vovka

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,40 +12,7 @@ namespace TRPZ2
         public MainWindow()
         {
             InitializeComponent();
-		lb1.Items.Add("Volodymyr Yakymenko");
-            lb1.Items.Add("Anna Heleshko");
-
-            lb1.Items.Add("Stas Logvynskyi");
-            lb1.Items.Add("Oleksiy Okorokov");
-            lb1.Items.Add("Daniil Zhdanovvv");
-            lb1.Items.Add("Dmitrii Nieliepin");
-
-            lb1.Items.Add("Max Vavryniuk");
-            lb1.Items.Add("Andrey Kolomets");
-            lb1.Items.Add("Yana Yushchuk");
-            lb1.Items.Add("Denys Sorokin");
-            lb1.Items.Add("Danil Melnik");
-
-            lb1.Items.Add("Karyna Sobkovska");
-
-
-
-	        lb1.Items.Add("Denys Grynenko");
-		    lb1.Items.Add("Yevhenii Borysenko");
-		    lb1.Items.Add("Max Vavryniuk");
-	        lb1.Items.Add("Volodymyr Danchuk");
-            lb1.Items.Add("Huz Bohdan");
-	        lb1.Items.Add("Denys Grynenko");
-            lb1.Items.Add("Yevhenii Borysenko");
-            lb1.Items.Add("Maksym Melnyk");
-            lb1.Items.Add("Yaroslav Andrushchenko");
-	        lb1.Items.Add("Denys Grynenko");
-		    lb1.Items.Add("Yevhenii Borysenko");
-            lb1.Items.Add("Vladimir Fokow");
-            lb1.Items.Add("Yevhenii Borysenko");
-            lb1.Items.Add("Oleksii Kravchenko");
-
-            lb1.Items.Add("Volodymyr Tolubko");
+		lb1.Items.Add("Volodymyr Yakymenko");     
         }
     }
 }
