e2d32a61506339c62ee0676767e600cfed7ad2dd
JustVovan4ik <danchukv.off1cial@gmail.com>
2022-10-06 14:53:01 +0300

    add my name

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,6 +1,7 @@
-﻿Class MainWindow
+Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         listBox1.Items.Add("Mykola Kutsyk")
         listBox1.Items.Add("Vadim Kostyanko")
+	listBox1.Items.Add("Danchuk Volodymyr")
     End Sub
 End Class
