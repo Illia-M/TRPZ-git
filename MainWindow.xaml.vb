3cbcba4b1e518134f8966f4589d7c3f395c3b83c
Yaroslav Andrushchenko <yarxslav1109@gmail.com>
2022-10-06 15:26:30 +0300

    added Andrushchenko

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -5,24 +5,15 @@ Class MainWindow
         listBox1.Items.Add("Vadim Kostyanko2")
         listBox1.Items.Add("Vadim Prohorchuk")
         listBox1.Items.Add("Daniil Zhdanov")
-	listBox1.Items.Add("Vitalii Panchenko")
+	    listBox1.Items.Add("Vitalii Panchenko")
         listBox1.Items.Add("Vitalii Panchenko")
         listBox1.Items.Add("Volodymyr Sharkovskyi")
         listBox1.Items.Add("Vitalii Panchenko")
-
         listBox1.Items.Add("Denys Grynenko")
-
         listBox1.Items.Add("Volodymyr Yakymenko")
-
         listBox1.Items.Add("Yaroslav Andrushchenko")
-
         listBox1.Items.Add("Volodymyr Tolubko")
-<<<<<<< HEAD
-
-=======
         listBox1.Items.Add("Oleksiy Kravchenko")
->>>>>>> 8d279a3478d9df93e6bcdca15cc3ec266fa5efcb
-	listBox1.Items.Add("Volodymyr Danchuk")
-
+	    listBox1.Items.Add("Volodymyr Danchuk")
     End Sub
 End Class
