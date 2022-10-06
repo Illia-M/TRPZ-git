409abe7cd9c76ddf6a7bd4c278363354a4270003
illia-m <illia@maier.page>
2022-10-06 15:15:59 +0300

    Revert "Vovka merged"
    This reverts commit 1c6fd8d510d8eb7aeb8c4d4e55c9bef8ed6b958f, reversing
    changes made to 31e42d1a993eed3da6c5e8faa903fb86b1694444.
    
    # Conflicts:
    #	MainWindow.xaml.vb

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -10,7 +10,6 @@
         listBox1.Items.Add("Vitalii Panchenko")
         listBox1.Items.Add("Danchuk Volodymyr")
         listBox1.Items.Add("Volodymyr Yakymenko")
-        listBox1.Items.Add("Yaroslav Andrushchenko")
         listBox1.Items.Add("Volodymyr Tolubko")
     End Sub
 End Class
