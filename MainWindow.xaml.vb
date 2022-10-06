0d4f7566223b70444252c09c74745a7bd655a83c
Freemankon <vtol2003@gmail.com>
2022-10-06 15:11:55 +0300

    My new b

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,5 +1,5 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
-        listBox1.Items.Add("FirstName LastName")
+        listBox1.Items.Add("Volodymyr Tolubko")
     End Sub
 End Class
