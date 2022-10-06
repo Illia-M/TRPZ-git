e351029896d7b18a9cef9edd4836d4d5944ffe1e
VadimKost <39429087+VadimKost@users.noreply.github.com>
2022-10-06 14:43:56 +0300

    lab1_commit

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,5 +1,5 @@
 ﻿Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
-        listBox1.Items.Add("FirstName LastName")
+        listBox1.Items.Add("Vadim Kostyanko")
     End Sub
 End Class
