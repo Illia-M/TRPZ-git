2c35e7dfb87d215e007ba9fbb493d27ee0eb2280
egortitov <70881645+egortitov@users.noreply.github.com>
2022-10-13 09:38:16 +0300

    lab1

--- hello.pl
+++ hello.pl
@@ -4,10 +4,10 @@ use strict;
 use warnings;
 
 print("Hello World\n");
-<<<<<<< HEAD
+
 print("FirstName LastName\n");
 print("Karyna Sobkovska\n");
-=======
-print("Sophiia Liemieshova\n");
-print("Yehor Titov\n");
 
+print("Sophiia Liemieshova\n");
+
+print("Yehor Titov\n");
