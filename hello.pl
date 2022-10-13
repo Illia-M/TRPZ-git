f04ae8864824320f357265b86e5aba8e634f9461
illia-m <illia@maier.page>
2022-10-13 09:54:26 +0300

    Revert "merged"
    This reverts commit 888fb832c1e831e57d0d063495e5c12afdcc08c0.

--- hello.pl
+++ hello.pl
@@ -7,10 +7,11 @@ print("Hello World\n");
 print("FirstName LastName\n");
 print("Diana Kornieieva\n");
 print("Sophiia Liemieshova\n");
-print("Yehor Titov\n");
 
 
 print("Karyna Sobkovska\n");
+
+print("Yehor Titov\n");
 
 
 print("Olexandr Opalatenko\n");
