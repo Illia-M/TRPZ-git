11684d8c1d639877cf258a8f5e57f7dc410850ae
illia-m <illia@maier.page>
2022-10-13 09:38:29 +0300

    Revert "merge"
    This reverts commit bae723796c8820bc416cf0725e7ff64ce97f1e2b, reversing
    changes made to 978d19375ac3d82c1f12a7727cb6852a6a0960ab.

--- hello.pl
+++ hello.pl
@@ -7,7 +7,6 @@ print("Hello World\n");
 print("FirstName LastName\n");
 
 print("Sophiia Liemieshova\n");
-print("Diana Kornieieva\n");
 
 
 print("Karyna Sobkovska\n");
@@ -19,4 +18,3 @@ print("Olexandr Opalatenko\n");
 
 print("FirstName LastName\n");
 print("Karyna Sobkovska\n");
->>>>>>> 978d19375ac3d82c1f12a7727cb6852a6a0960ab
