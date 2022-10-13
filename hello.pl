a0d397408bf571f7a95a54c8e2841f362c5da550
AlexSage02 <opalat2002@gmail.com>
2022-10-13 10:03:34 +0300

    1

--- hello.pl
+++ hello.pl
@@ -4,24 +4,12 @@ use strict;
 use warnings;
 
 print("Hello World\n");
-<<<<<<< HEAD
 print("FirstName LastName\n");
 print("Diana Kornieieva\n");
 print("Sophiia Liemieshova\n");
-
-
 print("Karyna Sobkovska\n");
-
 print("Yehor Titov\n");
-
-
 print("Olexandr Opalatenko\n");
-
-print("FirstName LastName\n");
-print("Karyna Sobkovska\n");
 print("Kondrashchenko Vadim\n");
-
-
-
 print("Kiselevych Anton\n");
 
