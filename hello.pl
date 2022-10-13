422732f37cdcebd12d15e7453050a7faef0996c2
illia-m <illia@maier.page>
2022-10-13 09:24:37 +0300

    Fix

--- hello.pl
+++ hello.pl
@@ -4,10 +4,6 @@ use strict;
 use warnings;
 
 print("Hello World\n");
-<<<<<<< HEAD
 print("FirstName LastName\n");
-print("Karyna Sobkovska\n");
-=======
 print("Sophiia Liemieshova\n");
-print("Yehor Titov\n");
 
