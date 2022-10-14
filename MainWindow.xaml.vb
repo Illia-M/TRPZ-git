b92fcccfad97476b356ba3cbaa5dfe6378775748
Alekseyrb <kravchenko.a.a.rb.gmail.com>
2022-10-14 14:27:32 +0300

    Oleksii Kravchenko

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -5,13 +5,19 @@
         listBox1.Items.Add("Vadim Kostyanko2")
         listBox1.Items.Add("Vadim Prohorchuk")
         listBox1.Items.Add("Daniil Zhdanov")
-	    listBox1.Items.Add("Vitalii Panchenko")
+	listBox1.Items.Add("Vitalii Panchenko")
+        listBox1.Items.Add("Vitalii Panchenko")
         listBox1.Items.Add("Volodymyr Sharkovskyi")
         listBox1.Items.Add("Vitalii Panchenko")
-        listBox1.Items.Add("Danchuk Volodymyr")
+
+        listBox1.Items.Add("Denys Grynenko")
+
         listBox1.Items.Add("Volodymyr Yakymenko")
+
         listBox1.Items.Add("Yaroslav Andrushchenko")
+
         listBox1.Items.Add("Volodymyr Tolubko")
-        listBox1.Items.Add("Oleksiy Kravchenko")
+        listBox1.Items.Add("Oleksii Kravchenko")
+        
     End Sub
 End Class
