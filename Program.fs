f7067d4b3eb84b43a74c7630f2ca5fe356b9b055
loviacheda <ksobkovska@gmail/com>
2022-10-20 08:44:14 +0300

    message

--- Program.fs
+++ Program.fs
@@ -9,7 +9,9 @@ let from whom =
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
-    let student = from "#1 FirstName LastName" // Call the function
+    let student = from "#1 FirstName LastName" 
+// Call the function
+ let student = from "#23 Karyna Sobkovska"
     printfn "Hello world %s" message
     printfn "Hello %s" student
     0 // return an integer exit code
