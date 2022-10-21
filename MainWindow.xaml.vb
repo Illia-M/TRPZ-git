19df24e7a06b908973683868f1734d0e915ea072
Gr1 <denisdmitrevich2@gmail.com>
2022-10-21 16:48:22 +0300

    lab1

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,27 +1,6 @@
 Class MainWindow
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
-        listBox1.Items.Add("Mykola Kutsyk")
-        listBox1.Items.Add("Vadim Kostyanko")
-        listBox1.Items.Add("Vadim Kostyanko2")
-        listBox1.Items.Add("Vadim Prohorchuk")
-        listBox1.Items.Add("Daniil Zhdanov")
-	listBox1.Items.Add("Vitalii Panchenko")
-        listBox1.Items.Add("Vitalii Panchenko")
-        listBox1.Items.Add("Volodymyr Sharkovskyi")
-        listBox1.Items.Add("Vitalii Panchenko")
-
         listBox1.Items.Add("Denys Grynenko")
-
-        listBox1.Items.Add("Volodymyr Yakymenko")
-
-        listBox1.Items.Add("Yaroslav Andrushchenko")
-
-        listBox1.Items.Add("Volodymyr Tolubko")
-
-        listBox1.Items.Add("Oleksiy Kravchenko")
-	listBox1.Items.Add("Volodymyr Danchuk")
-        listBox1.Items.Add("Dmytro Tkachenko")
-	    listBox1.Items.Add("Olia Derkach")
 
     End Sub
 End Class
