9f8406bdcf31c35870aea48d89582dc0cdeb4c37
Fnat1k <ilyaerkin11@gmail.com>
2023-02-23 10:42:58 +0200

    commit

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Illia_Yerkin")
+        Console.WriteLine("Illia Yerkin")
     End Sub
-End Module
+End Modul
