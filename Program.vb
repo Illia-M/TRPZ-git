88927d67e5b6a87c8a025db066b204372f0107f5
mrOvdMax <124711300+mrOvdMax@users.noreply.github.com>
2023-02-23 09:48:05 +0200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("FirstName LastName")
+        Console.WriteLine("Maxim Ovdiienko")
     End Sub
 End Module
