3334d3a4b91939a9299b410cffe802fb3c1fa8bf
SKotsiubynska <126114609+SKotsiubynska@users.noreply.github.com>
2023-02-23 10:24:20 +0200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Pron Yana")
+        Console.WriteLine("Kotsiubynska Sofia")
     End Sub
 End Module
