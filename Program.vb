2416c5891aa88584cc420256fe85241b2efcc74e
Oleksiy123 <126140783+Oleksiy123@users.noreply.github.com>
2023-02-23 11:11:17 +0200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Figol Vladislava")
+        Console.WriteLine("Gabrys Oleksiy")
     End Sub
 End Module
