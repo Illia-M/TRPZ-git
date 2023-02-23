20f7bc953b4423523f4add6d0fac0de403a644a1
Katyamyronets <katyamyroneta15@gmail.com>
2023-02-23 10:37:41 +0200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Pron Yana")
+        Console.WriteLine("Katya Myronets")
     End Sub
 End Module
