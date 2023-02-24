158328f6170b1ecb15bda35f7b1275da2338dbb1
Artem Matok <artemmatok@gmail.com>
2023-02-24 09:18:32 +0200

    Change

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Matok Artem")
+        Console.WriteLine("Artem Matok")
     End Sub
 End Module
