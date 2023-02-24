cebf86756543ea576a21f2a65bf91c5d9922c7d2
Danylo Honcharov <danilkogonchar@gmail.com>
2023-02-24 14:20:25 +0200

    Editing the file

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Nataliia Petrovych")
+        Console.WriteLine("Honcharov Danylo")
     End Sub
 End Modul
