394a3924fc7d265283154bc81f5a973654c78fe9
Dianadel25 <ddelita2@gmail.com>
2023-02-25 12:44:28 +0200

    changes in the file

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Datsenko Anastasia")
+        Console.WriteLine("Delita Diana")
     End Sub
 End Modul
