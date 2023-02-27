b539e91e33854eef75289c03214a5db14de47474
KostiukAS <kostiukandrey2005@gmail.com>
2023-02-28 00:54:09 +0200

    Name change
    Try 2

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Andrii Kostiuk")
+        Console.WriteLine("Andrii Kostiuk ")
     End Sub
 End Modul
