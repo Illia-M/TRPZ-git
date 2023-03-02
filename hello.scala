296e342ab597b40a841188903a8103ef6f5ba5b9
VladaFigol <126143399+VladaFigol@users.noreply.github.com>
2023-03-02 10:05:54 +0200

    Merge branch 'stream/IC-23-2' of https://github.com/Illia-M/TRPZ-git into stream/IC-23-2

--- hello.scala
+++ hello.scala
@@ -1,10 +1,12 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-
+    println("Vladislava Figol")
+    println("Illia_Yerkin")
     println("Artur Deundyak")
     println("Illia Maier")
     println("Maxim Ovdiienko")
-
+    println("Vladyslav Liubashenko!")
+    println("Iryna Stankova")
   }
 }
