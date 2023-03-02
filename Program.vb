04eb7e9c0381a36378076f53304cd42a59075d5a
SKotsiubynska <kotsiubynska.sofia@lll.kpi.ua>
2023-03-02 10:34:11 +0200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Bogdan Gilevich")
+        Console.WriteLine("Sofia Kotsiubynska")
     End Sub
 End Modul
