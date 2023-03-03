5d12b7cb1707fd29c7255176098272e9bda9283e
Guest057 <124689656+Guest057@users.noreply.github.com>
2023-03-03 09:28:54 +0200

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Oleksii_Gabris" << endl;
+    cout << "Vadym Fil" << endl;
+    cout << "Vasyl Shakhov" << endl;
+    cout << "Julia_Barna" << endl;
+    cout << "Sergiy_Reshetnyak" << endl;
+    cout << "Andrew_Kovalchuk" << endl;
+    cout << "Mokliuk Maxym" << endl;
+    cout << "Yaroslav Koval" << endl;
+}
