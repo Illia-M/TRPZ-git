ec447b9b250803965a413473d105da756cba3c2f
Alina <tokarskaaalina@gmail.com>
2023-03-05 09:52:31 +0200

    Added new name

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Andrii Kostiuk ")
+        Console.WriteLine("Alina Tokarska ")
     End Sub
 End Modul
