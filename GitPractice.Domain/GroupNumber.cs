5ad43256fdcd8f51aab0242cf7b025b7c558eead
Illia Maier <illia@maier.page>
2023-03-09 09:36:44 +0200

    Add task project

---
+++ GitPractice.Domain/GroupNumber.cs
@@ -0,0 +1,9 @@
+﻿public record GroupNumber
+{
+    public string Value { get; }
+
+    public GroupNumber(string value)
+    {
+        Value = value;
+    }
+}
---
+++ GitPractice.Domain/StudentsProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace TRPZ_git
+{
+    public class StudentsProvider
+    {
+        private ImmutableDictionary<GroupNumber, ImmutableList<Student>> _students;
+
+        public StudentsProvider()
+        {
+            _students = new Dictionary<GroupNumber, ImmutableList<Student>>()
+            {
+                { new GroupNumber("IC-21"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
+                { new GroupNumber("IC-22"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
+                { new GroupNumber("IC-23"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
+            }
+            .ToImmutableDictionary();
+        }
+    }
+}
+
