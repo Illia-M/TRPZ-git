e49c1f51020d9a7761d2d8f66b879ef2021a971d
Sanyo331 <dvershihora@gmail.com>
2023-03-13 23:21:52 +0200

    Update StudentsProvider.cs
    Added new student

--- GitPractice.Domain/StudentsProvider.cs
+++ GitPractice.Domain/StudentsProvider.cs
@@ -48,6 +48,7 @@ public class StudentsProvider
                     new Student("Kirill", "Nesterenko"),
                     new Student("Yana", "Pron"),
                     new Student("Yaroslav", "Dobrutskiy"),
+                    new Student("Oleksandr", "Vershyhora"),
                 }.ToImmutableList() },
             }
         .ToImmutableDictionary();
