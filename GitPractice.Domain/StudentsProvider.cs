486b56b3bd8fd2d3ee6ae139aeba6c82795f83ad
SofiaBielchik <bielchik.sofia@lll.kpi.ua>
2023-03-14 23:07:31 +0200

    added a student

--- GitPractice.Domain/StudentsProvider.cs
+++ GitPractice.Domain/StudentsProvider.cs
@@ -21,7 +21,8 @@ public class StudentsProvider
                     new Student("Daria", "Polishchuk"),
                     new Student("Andrew", "Kachalov"),
                     new Student("Bohdan", "Zaskaleta"),
-		    new Student("Yevhenii","Ihnatkin")
+		          new Student("Yevhenii","Ihnatkin"),
+                    new Student("Sofia","Bielchik") 
 
 
 
