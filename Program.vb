8e57d176c40230c7a8a0b7bae5f077f441aff9f7
Vadim <matok2004k@gmail.com>
2023-03-21 19:32:36 +0200

    Change

--- Program.vb
+++ Program.vb
@@ -3,6 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-        Console.WriteLine("Mato Vadim")
+        Console.WriteLine("Vadim Matok")
     End Sub
 End Module
