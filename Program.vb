b77e9caacf0c7b21e00f70d3d35ed0b57cfb1f6e
Ilya_Zhyrikov <zhyrikov.ilya@lll.kpi.ua>
2023-04-06 23:57:08 +0300

    Додавання рядка з іменем та прізвищем

--- Program.vb
+++ Program.vb
@@ -2,7 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
         Console.WriteLine("Ilya Zhyrikov")
     End Sub
-End Modul
+    End Modul
