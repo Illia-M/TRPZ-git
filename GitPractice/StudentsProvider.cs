1d0410b4e505d70d5b8186169dfe86f808647229
romkas228 <romandanuliyk@gmail.com>
2024-02-13 17:20:49 +0200

    Roman Danuliyk IC-31

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -18,7 +18,8 @@ public class StudentsProvider
                     new Student("Korotaiev", "Mykhailo"),
                     new Student("Driuk", "Vladyslav"),
                     new Student("Sofia", "Mykhailova"),
-                    new Student("Nikita","Uzhakov")
+                    new Student("Nikita","Uzhakov"),
+                    new Student("Roman","Danuliyk")
                     }.ToImmutableList()
                 },
                 { new GroupNumber("IC-32"), new[]
