39158b0698614a010efdaaccfe8acb135697b27d
artemRusachenko <142894412+artemRusachenko@users.noreply.github.com>
2024-02-13 12:51:16 +0200

    Update StudentsProvider.cs

--- GitPractice.Domain/StudentsProvider.cs
+++ GitPractice.Domain/StudentsProvider.cs
@@ -11,7 +11,7 @@ namespace TRPZ_git
         {
             _students = new Dictionary<GroupNumber, ImmutableList<Student>>()
             {
-                { new GroupNumber("IC-31"), new[]{new Student("Oleksandr", "Sychov"), new Student("Mikita", "Kalachuk"),new Student("Oleh", "Lobko") }.ToImmutableList(), new Student("Artem", "Rusachenko") }.ToImmutableList() },
+                { new GroupNumber("IC-31"), new[]{new Student("Oleksandr", "Sychov"), new Student("Mikita", "Kalachuk"),new Student("Oleh", "Lobko"), new Student("Artem", "Rusachenko") }.ToImmutableList() },
                 { new GroupNumber("IC-32"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
                 { new GroupNumber("IC-33"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
                 { new GroupNumber("IC-34"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
