6c5430e07a7ab11603f4370329cbcf76cd6c03cf
Illia Maier <illia@maier.page>
2024-02-13 14:08:02 +0200

    Add unit tests. Update solution. Add Github Actions

---
+++ GitPractice/StudentsProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+public class StudentsProvider
+{
+    private readonly ImmutableDictionary<GroupNumber, ImmutableList<Student>> _students;
+
+    public StudentsProvider()
+    {
+        _students = new Dictionary<GroupNumber, ImmutableList<Student>>()
+            {
+                { new GroupNumber("IC-31"), new[]
+                    {
+                    new Student("Oleksandr", "Sychov"),
+                    new Student("Mikita", "Kalachuk"),
+                    new Student("Oleh", "Lobko"),
+                    new Student("Illia", "Shmyhelskyi"),
+                    new Student("Artem", "Rusachenko")
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-32"), new[]
+                    {
+                    new Student("Dmytro", "Shlikhanov"),
+                    new Student("Olexandr", "Zhovmir"),
+                    new Student("Yuriy", "Cherepnya"),
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-33"), new[]
+                    {
+                    new Student("FirstName", "LastName")
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-34"), new[]
+                    {
+                    new Student("FirstName", "LastName")
+                    }.ToImmutableList()
+                },
+            }
+        .ToImmutableDictionary();
+    }
+
+    public IReadOnlyDictionary<GroupNumber, ImmutableList<Student>> Students => _students;
+}
