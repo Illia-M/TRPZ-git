5d780c71991deeead5342cdc6b63ef2d4a5d0410
Koval Bohdan <138917829+bohdan-kov@users.noreply.github.com>
2024-02-13 23:42:49 +0200

    Update StudentsProvider.cs

--- GitPractice.Domain/StudentsProvider.cs
+++ GitPractice.Domain/StudentsProvider.cs
@@ -1,23 +1,55 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 
-namespace TRPZ_git
+public class StudentsProvider
 {
-    public class StudentsProvider
+    private readonly ImmutableDictionary<GroupNumber, ImmutableList<Student>> _students;
+
+    public StudentsProvider()
     {
-        private ImmutableDictionary<GroupNumber, ImmutableList<Student>> _students;
-
-        public StudentsProvider()
-        {
-            _students = new Dictionary<GroupNumber, ImmutableList<Student>>()
+        _students = new Dictionary<GroupNumber, ImmutableList<Student>>()
             {
-                { new GroupNumber("IC-31"), new[]{new Student("Bohdan", "Koval") }.ToImmutableList() },
-                { new GroupNumber("IC-32"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
-                { new GroupNumber("IC-33"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
-                { new GroupNumber("IC-34"), new[]{new Student("FirstName", "LastName") }.ToImmutableList() },
-            }
-            .ToImmutableDictionary();
-        }
-    }
-}
+                { new GroupNumber("IC-31"), new[]
+                    {
+                    new Student("Oleksandr", "Sychov"),
+                    new Student("Mikita", "Kalachuk"),
+                    new Student("Oleh", "Lobko"),
+                    new Student("Illia", "Shmyhelskyi"),
+                    new Student("Artem", "Rusachenko"),
+                    new Student("Korotaiev", "Mykhailo"),
+                    new Student("Driuk", "Vladyslav"),
+                    new Student("Sofia", "Mykhailova"),
+                    new Student("Nikita","Uzhakov"),
+                    new Student("Yarslov","Shafinsky"),
+                    new Student("Anastasia","Danskaya"),
+                    new Student("Roman","Danuliyk"),
+                    new Student("Voloshyn","Artem"),
+                    new Student("Koval","Bohdan")
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-32"), new[]
+                    {
+                    new Student("Dmytro", "Shlikhanov"),
+                    new Student("Olexandr", "Zhovmir"),
+                    new Student("Yuriy", "Cherepnya"),
+                    new Student("Oleksandr", "Rudenko"),
+                    new Student("Maksym", "Zarytskyi"),
+                    new Student("Ihor", "Panchenko"),
 
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-33"), new[]
+                    {
+                    new Student("FirstName", "LastName")
+                    }.ToImmutableList()
+                },
+                { new GroupNumber("IC-34"), new[]
+                    {
+                    new Student("FirstName", "LastName")
+                    }.ToImmutableList()
+                },
+            }
+        .ToImmutableDictionary();
+    }
+
+    public IReadOnlyDictionary<GroupNumber, ImmutableList<Student>> Students => _students;
+}
