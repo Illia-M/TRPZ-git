3e1d77f679f68c6a4ca0e70b126adead523936ff
Koval Bohdan <bohdankoval3012@gmail.com>
2024-02-14 09:45:37 +0200

    Change

--- GitPractice.Domain/StudentsProvider.cs
+++ GitPractice.Domain/StudentsProvider.cs
@@ -34,7 +34,6 @@ public class StudentsProvider
                     new Student("Oleksandr", "Rudenko"),
                     new Student("Maksym", "Zarytskyi"),
                     new Student("Ihor", "Panchenko"),
-
                     }.ToImmutableList()
                 },
                 { new GroupNumber("IC-33"), new[]
