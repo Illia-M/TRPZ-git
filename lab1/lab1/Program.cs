6b1ea12fc4001c17f7d993a5127b0e23e90eacf0
Андрій Швед <andriy2shved@gmail.com>
2024-02-14 18:51:51 +0200

    Created a student object

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+            student.FirstName = "Andrew";
+            student.SurName = "Shved";
+        }
+    }
+}
---
+++ lab1/lab1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Student
+    {
+        public string FirstName;
+        public string SurName;
+    }
+}
