4b6b070a9586c5f61c7779851c71f440e073e08f
Andrii Shevchuk <shevchuk.andrii@lll.kpi.ua>
2024-02-14 10:06:20 +0200

    IS-31 Andrii Shevchuk

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -23,6 +23,7 @@ public class StudentsProvider
                     new Student("Anastasia","Danskaya"),
                     new Student("Roman","Danuliyk"),
                     new Student("Voloshyn","Artem"),
+                    new Student("Andrii", "Shevchuk")
                     }.ToImmutableList()
                 },
                 { new GroupNumber("IC-32"), new[]
