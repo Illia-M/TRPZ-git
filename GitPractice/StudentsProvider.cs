614ff855cfd5a1f6a7ee857dfab7ddd0cec43f88
longvodyneste <dima22statkevych@gmail.com>
2024-02-15 12:11:29 +0200

    student added

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -27,7 +27,8 @@ public class StudentsProvider
                     new Student("Roman","Danuliyk"),
                     new Student("Voloshyn","Artem"),
                     new Student("Maksym","Harmash"),
-                    new Student("Koval", "Bohdan")
+                    new Student("Koval", "Bohdan"),
+                    new Student("Dmytro","Statkevych"),
 
                     }.ToImmutableList()
                 },
