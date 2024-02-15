d6be148dd432bbb90c0a3d861c95f30c42ef93f3
Maria Zozulia <zozulua06@gmail.com>
2024-02-15 16:14:47 +0200

    I added myself to the list.

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -67,7 +67,8 @@ public class StudentsProvider
                     new Student("Artem", "Russu"),
                     new Student("Alex", "Hezha"),
                     new Student("Vlad", "Pohorilets"),
-                    new Student("Liudmyla", "Perekhrest")
+                    new Student("Liudmyla", "Perekhrest"),
+                    new Student("Mariia","Zozulya")
                     }.ToImmutableList()
                 },
             }
