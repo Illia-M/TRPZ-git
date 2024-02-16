be9619fec315aca8d2f3f51b5ce8a6221954f2c0
PesPatron007 <rybalko.maksym1@lll.kpi.ua>
2024-02-16 17:11:29 +0200

    new student

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -71,7 +71,8 @@ public class StudentsProvider
                     new Student("Bohdan", "Korniienko"),
                     new Student("Maksym", "Kurenkov"),
                     new Student("Anton", "Lykholit"),
-                    new Student("Anna", "Morina")
+                    new Student("Anna", "Morina"),
+                    new Student("Maksym", "Rybalko")
                     }.ToImmutableList()
                 },
                 { new GroupNumber("IC-34"), new[]
