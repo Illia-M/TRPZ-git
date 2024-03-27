43a3879f2af0345c6c8c1d000f84e3b73f49ba8c
MoguchiyDuh <123489538+MoguchiyDuh@users.noreply.github.com>
2024-03-28 00:27:46 +0300

    Update StudentsProvider.cs

--- GitPractice/StudentsProvider.cs
+++ GitPractice/StudentsProvider.cs
@@ -64,6 +64,7 @@ public class StudentsProvider
                     new Student("Mykhailo","Pylypenko"),
                     new Student("Dmytro","Reznik"),
                     new Student("Kyryl", "Kravets"),
+                    new Student("Ivan", "Ivanov"),
                     }.ToImmutableList()
                 },
                 { new GroupNumber("IC-33"), new[]
